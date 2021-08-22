deb696e67bcfdb5bc76fe55e40e5871c9d70b020
helloDestroyerOfWorlds <jakebronson89@gmail.com>
2021-08-22 13:33:37 -0400

    > fixing a quick error with last push

--- propeller_design_tools/__init__.py
+++ propeller_design_tools/__init__.py
@@ -1,2 +1,2 @@
-from user_settings import set_airfoil_database, set_propeller_database
+from propeller_design_tools.user_settings import set_airfoil_database, set_propeller_database
 from propeller_design_tools.funcs import *
